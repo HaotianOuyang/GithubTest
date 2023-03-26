8f6a6abebd293d27e4082c52d76b20c3508d00b4
HaotianOuyang <1319310146@qq.com>
2023-03-26 20:33:06 +1000

    Add work files

---
+++ GithubTest/JobCollection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+
+
+public class JobCollection : IJobCollection {
+	private IJob[] jobs;
+	private uint count;
+
+	public JobCollection( uint capacity ) {
+		if( !( capacity >= 1 ) ) throw new ArgumentException();
+		jobs = new IJob[capacity];
+        count = 0;
+	}
+
+	public uint Capacity {
+		get { return (uint) jobs.Length; }
+	}
+
+	public uint Count {
+		get { return count; }
+	}
+
+	public bool Add( IJob job ) {
+        //To be implemented by students
+        if(Count < Capacity)
+        {
+            for (int i = 0; i < Capacity; i++)
+            {
+                if (jobs[i].Id == job.Id)
+                {                 
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+            //   Count = Count + 1;
+            // Capacity = Capacity;
+        }
+        throw new System.NotImplementedException();
+    }
+
+    public bool Contains(uint id) {
+        //To be implemented by students
+        throw new System.NotImplementedException();
+
+    }
+
+    public IJob? Find( uint id ) {
+        //To be implemented by students
+        throw new System.NotImplementedException();
+
+    }
+
+    public bool Remove(uint id) {
+        //To be implemented by students
+        throw new System.NotImplementedException();
+    }
+
+    public IJob[] ToArray() {
+        //To be implemented by students
+        throw new System.NotImplementedException();
+    }
+}
